f743e43d8042df4cab4175354f80bc23e29c4cd2
Mark Kruse87 <a19mkruse87k@outlook.com>
2021-06-23 21:50:56 -0800

    Update Wildnet2 Create .js file create html file for javascript practice

---
+++ 21s-external.js
@@ -0,0 +1,4 @@
+function mySavedFunction() {
+    document.getElementById("demo9").innerHTML = "Saved script used to change the paragraph Notice the script tags are not saved in the .js file"
+    
+}
